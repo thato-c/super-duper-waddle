ec292442d1b36ab27938cec4f51940b8221642df
Thato <t.chitja369@gmail.com>
2023-09-22 12:11:22 +0200

    Connect StudentController to the ApplicationContext

---
+++ waddle/waddle/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using waddle.Data;
+
+namespace waddle.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public StudentController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
